47db52915c79f79cb327106445751e2cf5aa9df3
shubham somavamshi <shubhamraopatil123@gmail.com>
2025-10-24 23:04:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+//your JS code here. If required
+    let user = confirm("Do you want to proceed?");
+       if(user){
+        alert("You clicked OK. Proceeding...")
+       }else{
+        alert("You clicked Cancel. Exiting...")
+       }
